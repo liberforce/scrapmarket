6ed11a94fdd15678457a86bd3518168db6946d58
Luis Menina <liberforce@freeside.fr>
2024-05-10 01:39:52 +0200

    Fix request to not get a 403

--- get-card-prices.py
+++ get-card-prices.py
@@ -22,9 +22,17 @@ def get_product(client, url):
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Language": "fr-FR,fr;q=0.8,en-US;q=0.5,en;q=0.3",
-        "Accept-Encoding": "text",  # "gzip, deflate, br",
+        # "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Encoding": "text",
+        "Referer": "https://www.cardmarket.com/en/Magic/Products/Singles/Onslaught/Tribal-Golem?sellerCountry=12&language=2&minCondition=2",
+        "DNT": "1",
         "Connection": "keep-alive",
-        "Cookie": "cf_clearance=3GmL3uJX9q2JxTZEOZOwVKm.WqKFJquyd0GbJbGhK98-1713021396-1.0.1.1-Uc33WFLb4B8Wf7l01LpeDCTJy9uvWyqgnHi84PX0Eume9DbXKtAXdAMggjiPC41SBasSb1a5WSrpFpoX6uTGOA; _cfuvid=rM5T_G7UTMbiFfAJUhsOGl5XOTqNKH.eSZDTNbPc9xM-1715295689955-0.0.1.1-604800000; PHPSESSID=mk12v0lc913ragbsc7rteakde8; __cf_bm=7mJdA0wutFGzZ1ucxf62aU2AOd9Z0QaX4UMQWdQHWEM-1715295689-1.0.1.1-pAKpp5xsqimzNWqr4WKfImseXqukn5ErMCaRxu8srCRMWWpnz.ZttUgbkw48WBadRFbIbRYJo4gwL5bpQsI1_Q",
+        "Cookie": "cf_clearance=3GmL3uJX9q2JxTZEOZOwVKm.WqKFJquyd0GbJbGhK98-1713021396-1.0.1.1-Uc33WFLb4B8Wf7l01LpeDCTJy9uvWyqgnHi84PX0Eume9DbXKtAXdAMggjiPC41SBasSb1a5WSrpFpoX6uTGOA; _cfuvid=AGZW3RgNz11Fz6w7jhmoJuXm6Sc_hDQtOAfr0xBjKZg-1715295882066-0.0.1.1-604800000; PHPSESSID=mk12v0lc913ragbsc7rteakde8; __cf_bm=thQ5VwZLoS9psGrWHz7n9V.Dy_AVul_rsmDxq1ZFVus-1715297619-1.0.1.1-Rwdpl7jktiDY1woRThlNfT16TO4CPr7RNhbKLs8rh4x4kDYePYa1OxB03eQUWOrfTRSeCtdHzUmzfXm0vDNPlA",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "same-origin",
+        "TE": "trailers",
     }
     result = client.send_request(method, url, headers=headers, params=payload)
 
