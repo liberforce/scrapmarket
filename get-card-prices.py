a70ece66c3fce366f5b75002048ad3f90def5dbd
Luis Menina <liberforce@freeside.fr>
2024-05-10 04:53:57 +0200

    Extract prices from sellers

--- get-card-prices.py
+++ get-card-prices.py
@@ -1,4 +1,5 @@
 #! /bin/env python3
+from pprint import pprint
 import re
 from enum import Enum
 import dotenv
@@ -20,7 +21,7 @@ def is_article_row(tag) -> bool:
     return tag.has_attr("id") and tag.get("id").startswith("articleRow")
 
 
-def get_product(client, url):
+def get_product_table(client, url):
     method = "GET"
     payload = {"sellerCountry": 12, "language": 2, "minCondition": 2}
     headers = {
@@ -48,10 +49,60 @@ def get_product(client, url):
         f.write(result.text)
 
     soup = BeautifulSoup(result.text, features="html.parser")
-    matches = soup.find_all(id=re.compile(r"articleRow\d+"))
+    soup_rows = soup.find_all(id=re.compile(r"articleRow\d+"))
 
-    for match in matches:
-        print(match.prettify())
+    rows = []
+    for soup_row in soup_rows:
+        row = soup_row.get_text(";").split(";")
+        rows.append(row)
+
+    return rows
+
+
+def interpret_product_row(product_name: str, row: list) -> dict[str, list]:
+    fields = {}
+    fields["sales"] = row.pop(0)
+
+    if row[0] == "K":
+        fields["powerseller"] = True
+        row.pop(0)
+    else:
+        fields["powerseller"] = False
+
+    fields["seller_name"] = row.pop(0)
+    fields["grading"] = row.pop(0)
+    assert fields["grading"] in ("MT", "NM", "EX", "GD", "LP", "PL", "PO")
+    fields["quantity"] = row.pop(-1)
+    assert fields["quantity"].isdigit()
+    fields["price"] = row.pop(-1)
+    assert "€" in fields["price"]
+
+    seller = {
+        "name": fields["seller_name"],
+        "sales": fields["sales"],
+    }
+    offers = [
+        {
+            "product_name": product_name,
+            "grading": fields["grading"],
+            "price": fields["price"],
+            "quantity": fields["quantity"],
+        }
+    ]
+    return {seller["name"]: offers}
+
+
+def interpret_product_table(product_name: str, table: list[list]) -> dict[dict, list]:
+    product_by_sellers = {}
+    for row in table:
+        product = interpret_product_row(product_name, row)
+        seller = list(product)[0]
+        if seller in product_by_sellers:
+            product_by_sellers[seller].extend(product[seller])
+        else:
+            product_by_sellers[seller] = product[seller]
+
+    return product_by_sellers
 
 
 class MtgSetId(Enum):
@@ -87,12 +138,18 @@ def get_product_url(
 def main():
     dotenv.load_dotenv()
     client = Client()
+    product_name = "tribal golem"
     url = get_product_url(
         products.ProductType.CARD,
-        product_name="tribal golem",
+        product_name=product_name,  # TODO: use a dataclass with properties to normalize
         set_id=MtgSetId.ONS,
     )
-    get_product(client, url)
+    raw_product_table = get_product_table(client, url)
+    product_by_sellers = interpret_product_table(
+        normalize_product_name(product_name),
+        raw_product_table,
+    )
+    pprint(product_by_sellers)
 
 
 if __name__ == "__main__":
