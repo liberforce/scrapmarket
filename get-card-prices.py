c119d6ec906cf6349b56d244c2813856305af0b9
Luis Menina <liberforce@freeside.fr>
2024-06-06 23:18:16 +0200

    Make input products file configurable

--- get-card-prices.py
+++ get-card-prices.py
@@ -1,4 +1,5 @@
 #! /bin/env python3
+import argparse
 import json
 import logging
 import os.path
@@ -16,24 +17,37 @@ def take(n, iterable):
     return islice(iterable, n)
 
 
+def get_cmdline_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "product_filename",
+        help="The file containing the product information",
+    )
+    return parser.parse_args()
+
+
 def main():
     dotenv.load_dotenv()
     client = Client()
     products = []
+    args = get_cmdline_args()
 
-    if not os.path.exists("fastlands-offers.json"):
-        with open("fastlands.json") as fp:
-            products_data = json.load(fp)
+    with open(args.product_filename) as fp:
+        products_data = json.load(fp)
 
+    base, ext = os.path.splitext(args.product_filename)
+    offers_filename = f"{base}-offers{ext}"
+
+    if not os.path.exists(offers_filename):
         products = use_cases.search_products(client, products_data, should_raise=True)
         assert all(products), products
 
         multiproduct_offers = use_cases.get_multiproduct_offers(client, products)
 
-        with open("fastlands-offers.json", "w") as fp:
+        with open(offers_filename, "w") as fp:
             json.dump(multiproduct_offers, fp, indent=4, sort_keys=True)
     else:
-        with open("fastlands-offers.json") as fp:
+        with open(offers_filename, "r") as fp:
             multiproduct_offers = json.load(fp)
 
     # Get offers for each product, sorted by best offer
